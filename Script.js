5a7288aa4d2c60e1d53f4cb1dc3b3d98386ee8c1
Robert Bays <bays.robert@gmail.com>
2016-06-13 15:10:03 +0100

    clickable button to change text

--- Script.js
+++ Script.js
@@ -1,3 +1,17 @@
-//window.alert("Hello World");
-function test(){
-document.getElementById("demo").innerHTML = "This is the filler text";}
+		window.addEventListener('load',myFunction,false);
+		
+		function myFunction(){
+			document.getElementById("demo").innerHTML = "Hello world";
+		}
+		var whey = document.getElementById("whey");
+		whey.innerHTML = "test thing?";
+
+
+
+		var button1 = document.getElementById("button1");
+		button1.addEventListener('click',text_update,false);
+		
+		function text_update(){
+			var userinput = document.getElementById("input1").value;
+			document.getElementById("button1").innerHTML = userinput;
+		}
